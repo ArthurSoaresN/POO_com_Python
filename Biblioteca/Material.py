ec8afe25e4cc223a7820d4beb5b41c8a0bc9b9b6
Arthur Soares <arthursoaressi@hotmail.com>
2025-08-20 21:35:33 -0300

    Update Material.py

--- Biblioteca/Material.py
+++ Biblioteca/Material.py
@@ -1,1 +1,10 @@
 # Classe Pai
+
+# Atributos:
+# Titulo da Revista ou Livro
+# Origem Autor ou Editora
+# Ano de publicação
+
+class Material:
+    def __init__(self, titulo: str, origem: str, ano: int):
+        self.__titulo = titulo
