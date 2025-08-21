37bd6a61f85b07db4c65b9aa0416cf6dc0ce5574
Arthur Soares <arthursoaressi@hotmail.com>
2025-08-20 21:46:40 -0300

    livro.py

--- Biblioteca/livro.py
+++ Biblioteca/livro.py
@@ -1,3 +1,28 @@
 # Classe Filha Livro
+# Livro herda de Material. Atributos adicionais: genero e numero_paginas.
 
 from material import Material
+
+class Livro(Material):
+    def __init__(self, titulo, origem, ano, genero:str, paginas:int):
+        super().__init__(titulo, origem, ano)
+        self.__genero = genero
+        self.__paginas = paginas
+
+    # METODOS GET
+
+    def getGenero(self):
+        return self.__genero
+    
+    def getPaginas(self):
+        return self.__paginas
+    
+    # METODOS SET
+
+    def setGenero(self, novo_genero):
+        self.__genero = novo_genero
+    
+    def setPaginas(self, novo_paginas):
+        self.__paginas = novo_paginas
+    
+    # Metodo
---
+++ Biblioteca/revista.py
@@ -0,0 +1,3 @@
+# Classe Filha revista
+
+from material import Material
