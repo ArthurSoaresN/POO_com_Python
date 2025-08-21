1f28995def107bb92f7c05b56330cb3362920328
Arthur Soares <arthursoaressi@hotmail.com>
2025-08-20 23:23:03 -0300

    Update biblioteca.py

--- Biblioteca/biblioteca.py
+++ Biblioteca/biblioteca.py
@@ -8,3 +8,29 @@
 # Se sim, remove do acervo e adiciona ao historico_emprestimos do usuario.
 # devolver_material(usuario, titulo): Faz o processo inverso.
 # consultar_acervo(): Exibe todos os materiais disponíveis no acervo, utilizando o método exibir_informacoes() de cada objeto.
+
+from material import Material
+from livro import Livro
+from revista import Revista
+from usuario import Usuario
+
+class Biblioteca:
+    def __init__(self, acervo: list, usuarios: list):
+        self.__acervo = acervo
+        self.__usuarios = usuarios
+
+    # GET
+
+    def getAcervo(self):
+        return self.__acervo
+    
+    def getUsuarios(self):
+        return self.__usuarios
+    
+    # 
+
+    def setAcervo(self, novo_acervo: list):
+        self.__acervo = novo_acervo
+
+    def setUsuarios(self, novo_usuarios: list):
+        self.__usuarios = novo_usuarios
