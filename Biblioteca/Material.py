b62b0abb50dae6eed555019a40bcfbbaefea37ba
Arthur Soares <arthursoaressi@hotmail.com>
2025-08-20 21:42:14 -0300

    rename

--- Biblioteca/Material.py
+++ Biblioteca/Material.py
@@ -5,6 +5,40 @@
 # Origem Autor ou Editora
 # Ano de publicação
 
+# Metodo:
+# exibir_informacoes, mostrará quais são os atributos, deve ser implementado com po
+
 class Material:
     def __init__(self, titulo: str, origem: str, ano: int):
         self.__titulo = titulo
+        self.__origem = origem
+        self.__ano = ano
+
+    # METODOS GET
+
+    def getTitulo(self):
+        return self.__titulo
+    
+    def getOrigem(self):
+        return self.__origem
+    
+    def getAno(self):
+        return self.__ano
+    
+    # METODOS SET
+
+    def setTitulo(self, novo_titulo: str):
+        self.__titulo = novo_titulo
+
+    def setOrigem(self, novo_origem: str):
+        self.__origem = novo_origem
+    
+    def setAno(self, novo_ano: int):
+        self.__ano = novo_ano
+
+    # Metodo
+
+    def ExibirInfo(self):
+        print(f'Titulo: {self.__titulo}')
+        print(f'Origem: {self.__origem}')
+        print(f'Ano de Postagem: {self.__ano}')
---
+++ Biblioteca/livro.py
@@ -0,0 +1,3 @@
+# Classe Filha Livro
+
+from material import Material
